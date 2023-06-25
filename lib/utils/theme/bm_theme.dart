ab23ee21658f6c2db89d521af540495f984fbdb1
razadi <razadi07@gmail.com>
2023-06-25 05:52:54 -0600

    inicial de bepensa

---
+++ lib/utils/theme/bm_theme.dart
@@ -0,0 +1,38 @@
+export 'package:bepensa_unidades/utils/theme/bm_colors.dart';
+import 'package:flutter/material.dart';
+
+class BmTheme {
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: false,
+        fontFamily: 'Roboto',
+        primaryColor: BmColors.colorPrimaryInterfaz,
+        primaryColorLight: BmColors.colorPrimaryInterfaz,
+        brightness: Brightness.light,
+        textTheme: const TextTheme(
+          labelSmall: TextStyle(
+              fontSize: 16,
+              color: BmColors.colorTertiaryBody,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.51),
+          bodySmall: TextStyle(
+              fontSize: 14,
+              color: BmColors.colorComplementary2,
+              fontStyle: FontStyle.normal,
+              letterSpacing: 0.48),
+          displaySmall: TextStyle(
+              fontSize: 16,
+              color: BmColors.colorWhite,
+              fontStyle: FontStyle.normal,
+              letterSpacing: 0.51),
+        ),
+        appBarTheme: const AppBarTheme(
+            color: BmColors.colorComplementary2,
+            foregroundColor: BmColors.colorWhite),
+        colorScheme: const ColorScheme.light(
+          onPrimary: BmColors.colorPrimaryInterfaz,
+          primary: BmColors.colorPrimaryInterfaz,
+          secondary: BmColors.colorComplementary,
+          onSecondary: BmColors.colorComplementary,
+        ),
+      );
+}
---
+++ lib/utils/utils.dart
@@ -0,0 +1,2 @@
+export 'package:bepensa_unidades/utils/theme/bm_colors.dart';
+export 'package:bepensa_unidades/utils/theme/bm_theme.dart';
